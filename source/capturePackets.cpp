06c2b65cdce3fad769b6b5f370055bfc6560fa76
6disquiet <kpolcyn.n@gmail.com>
2023-07-06 22:42:44 +0200

    CapturingPackets:  * Added constructor and destructor  * Changed errbuf and handle to be class attributes  * Repaired capturePackets function

--- source/capturePackets.cpp
+++ source/capturePackets.cpp
@@ -2,19 +2,25 @@
 #include<pcap.h>
 
 void CapturePackets::capturePackets(){
-    char errbuf[PCAP_ERRBUF_SIZE];
-    auto handle = pcap_open_live(nullptr, 1, 1000, 10, errbuf);
     pcap_pkthdr* header;
-    std::cout << "pcap opened"<< '\n';
     const u_char* packet;
     while(int returnValue = pcap_next_ex(handle, &header, &packet) >=0){
-        if(returnValue == 0){
-            
+        if(returnValue == 0){  
             continue;
         }
+        std::cout<< returnValue;
         std::cout << "packet:\n";
         std::cout << packet;
     }
-    pcap_close(handle);
 
+}
+CapturePackets::CapturePackets(){
+    char errbuf[PCAP_ERRBUF_SIZE];
+    handle = pcap_open_live(nullptr, 1, 1000, 10, errbuf);
+    std::cout<<"Stream opened."<<'\n';
+}
+
+CapturePackets::~CapturePackets(){
+    pcap_close(handle);
+    std::cout<<"Stream closed."<<'\n';
 }
