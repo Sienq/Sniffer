af49e32052ac5ad53c7a005156c772cf44d14664
6disquiet <kpolcyn.n@gmail.com>
2023-11-02 12:49:41 +0100

    Saving cache into file:  * added method that saves cache into file after closing stream  * removed double free() at capturePackets destructor

--- include/capturePackets.hpp
+++ include/capturePackets.hpp
@@ -2,15 +2,18 @@
 #include<iostream>
 #include<memory>
 #include<vector>
+#include<string>
 
 class CapturePackets{
     private:
     pcap_t* handle;
     u_char* user;
+    std::vector<std::vector<u_char>> cache;
 
     public:
     void capturePackets();
     void show_packet(std::vector<u_char> stored_packet_vector);
+    void save_packet_into_file(const std::string&);
     CapturePackets();
     ~CapturePackets();
 
