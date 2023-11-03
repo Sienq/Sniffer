07715d592476f167d8989746b86998f12ac953cd
6devon <100228074+6devon@users.noreply.github.com>
2023-11-03 14:35:49 +0100

    Saving cache into file: (#18)
    * added method that saves cache into file after closing stream
     * removed double free() at capturePackets destructor

--- source/capturePackets.cpp
+++ source/capturePackets.cpp
@@ -3,15 +3,20 @@
 #include <cstring>
 #include <vector>
 #include <iomanip>
+#include <fstream>
 
 void CapturePackets::capturePackets()
 {
     pcap_pkthdr *header;
     const u_char *packet;
-    
+    int number_of_packets = 100;
+    int counter = 0;
 
     while (int returnValue = pcap_next_ex(handle, &header, &packet) >= 0)
-    {
+    {   
+        if(counter > 100){
+            break;
+        }
         if (returnValue == 0)
         {
             continue;
@@ -21,17 +26,39 @@ void CapturePackets::capturePackets()
         for(int i = 0; i < packet_size; i++){
             stored_packet_vector.push_back(packet[i]);
         }
+        cache.push_back(stored_packet_vector);
         show_packet(stored_packet_vector);
+        counter++;
     }
+    save_packet_into_file("out.txt");
 }
 
 void CapturePackets::show_packet(std::vector<u_char> stored_packet_vector)
 {
     for(int i = 0; i < stored_packet_vector.size(); i++){
-            std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(stored_packet_vector[i]) << " ";
+        std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(stored_packet_vector[i]) << " ";
         }
     std::cout << '\n';
 }
+
+void CapturePackets::save_packet_into_file(const std::string& filename)
+{
+    std::ofstream outfile(filename, std::ios::out | std::ios::binary);
+    if (!outfile.good()) {
+        std::cerr << "Error occured while opening file: " << filename << std::endl;
+        return;
+    }
+
+    for(int i = 0; i < cache.size(); i++){
+        for(int j = 0; j < cache[i].size(); j++){
+            outfile << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(cache[i][j]) << " ";
+        }
+        outfile << '\n';
+    }
+    outfile.close();
+    std::cout << "Packet saved to file: " << filename << std::endl;
+}
+
 
 CapturePackets::CapturePackets()
 {
@@ -43,6 +70,5 @@ CapturePackets::CapturePackets()
 CapturePackets::~CapturePackets()
 {
     pcap_close(handle);
-    free(handle);
     std::cout << "Stream closed." << '\n';
 }
