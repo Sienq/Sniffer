d4d563c92be514472c0abd07d00a2649d65dc54a
6devon <100228074+6devon@users.noreply.github.com>
2023-11-06 15:16:32 +0100

    Filter IP packets: (#20)
    * added method to filter packets by IP protocol
     * fixed code styling

--- source/capturePackets.cpp
+++ source/capturePackets.cpp
@@ -4,20 +4,21 @@
 #include <vector>
 #include <iomanip>
 #include <fstream>
+#include <filterPackets.hpp>
 
 void CapturePackets::capturePackets()
 {
     pcap_pkthdr *header;
     const u_char *packet;
-    int number_of_packets = 100;
+    int number_of_packets = 10;
     int counter = 0;
 
     while (int returnValue = pcap_next_ex(handle, &header, &packet) >= 0)
     {   
-        if(counter > 100){
+        if(counter > number_of_packets){
             break;
         }
-        if (returnValue == 0)
+        if(returnValue == 0)
         {
             continue;
         }
@@ -26,32 +27,43 @@ void CapturePackets::capturePackets()
         for(int i = 0; i < packet_size; i++){
             stored_packet_vector.push_back(packet[i]);
         }
+        FilterPackets f;
+        f.filterPacketEtherType(stored_packet_vector);
+        auto ether_type = f.getEtherType();
+        if(ether_type == "IPv4" or ether_type == "IPv6")
+        {
+            std::cout<<ether_type<<'\n';
+            counter++;
+        }
         save_packet_live(stored_packet_vector);
         cache.push_back(stored_packet_vector);
-        show_packet(stored_packet_vector);
-        counter++;
+        //show_packet(stored_packet_vector);
     }
     save_packet_into_file("out.txt");
 }
 
 void CapturePackets::show_packet(std::vector<u_char> stored_packet_vector)
 {
-    for(int i = 0; i < stored_packet_vector.size(); i++){
+    for(int i = 0; i < stored_packet_vector.size(); i++)
+    {
         std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(stored_packet_vector[i]) << " ";
-        }
+    }
     std::cout << '\n';
 }
 
 void CapturePackets::save_packet_into_file(const std::string& filename)
 {
     std::ofstream outfile(filename, std::ios::out | std::ios::binary);
-    if (!outfile.good()) {
+    if(!outfile.good()) 
+    {
         std::cerr << "Error occured while opening file: " << filename << std::endl;
         return;
     }
 
-    for(int i = 0; i < cache.size(); i++){
-        for(int j = 0; j < cache[i].size(); j++){
+    for(int i = 0; i < cache.size(); i++)
+    {
+        for(int j = 0; j < cache[i].size(); j++)
+        {
             outfile << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(cache[i][j]) << " ";
         }
         outfile << '\n';
@@ -63,7 +75,8 @@ void CapturePackets::save_packet_live(const std::vector<u_char>& stored_packet_v
 {
     std::ofstream outfile;
     outfile.open("live_data.txt", std::ios_base::app);
-    for(int i = 0; i < stored_packet_vector.size(); i++){
+    for(int i = 0; i < stored_packet_vector.size(); i++)
+    {
     outfile << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(stored_packet_vector[i]) << " ";
     }
     outfile << '\n';
